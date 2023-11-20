602393b755ea9ed8bb3cb140f4390e4aa4986145
beksus <85835904+beksus@users.noreply.github.com>
2023-11-20 15:54:13 +0800

    Create LibraryMain.py

---
+++ LibraryMain.py
@@ -0,0 +1,19 @@
+x = True
+
+while x:
+    print("OPTION: \n1 import books\n2 delete/edit\n3 display\n4 exit")
+    y = input("Enter option:")
+
+    if(y == '1'):
+        print('success')
+    elif(y == '2'):
+        print('success')
+    elif(y == '3'):
+        print('success')
+    elif(y == '4'):
+        print('ByeBye')
+        x = False
+    else:
+        print('failure!!! try again')
+
+
