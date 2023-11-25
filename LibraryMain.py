33dd7cf4480f89f93d0db4a2e250a1fb8b81e69f
beksus <85835904+beksus@users.noreply.github.com>
2023-11-25 19:59:09 +0800

    library system main
    idk

--- LibraryMain.py
+++ LibraryMain.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 x = True
 
 while x:
@@ -17,3 +18,24 @@ while x:
         print('failure!!! try again')
 print('template')
 
+=======
+from Book import Book
+
+isbn = "9780747532686"
+author = 'JK Rowling'
+title = "Harry Potter and the Philosopher's Stone"
+publisher = "Bloomsbury"
+genre = "Fantasy"
+date_published = '1997'
+date_purchased = '15-03-2020'
+type = 'to-read'
+
+#object book has a values above
+book = Book(isbn, author, title, publisher, genre,date_published,date_purchased,type)
+
+# you can call and get all values
+print(book.__str__())
+
+# you can call and get value with integer from 1 to 8
+print(book.getter(1))
+>>>>>>> Stashed changes
