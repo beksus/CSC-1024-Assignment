c34e7a58e77a77c2c819c004270a9e1ff3808c16
beksus <85835904+beksus@users.noreply.github.com>
2023-11-28 08:34:22 +0800

    beautify file

--- test.py
+++ test.py
@@ -5,22 +5,20 @@ file = open("text.txt", "r")
 # r* - reading and writing
 
 data = file.read()
-data.replace(',',' ')
+data.replace(',', ' ')
 
 data_into_list = data.split(",")
 
-new_list = [["damn" , 'shit']]
+new_list = [["damn", 'shit']]
 length = int(len(data_into_list)/4)
 for i in range(length):
-    if(i >= 1):
-        new_list.append(data_into_list[3 * i :7 * i])
+    if i >= 1:
+        new_list.append(data_into_list[3 * i:7 * i])
     else:
         new_list.append(data_into_list[:3])
     i += 1
 
-
-
-#print(data_into_list)
+# print(data_into_list)
 
 for i in new_list:
     print(i)
