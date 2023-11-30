055f38790ddb343cdb82c8ca5139ed5d5e651188
beksus <85835904+beksus@users.noreply.github.com>
2023-11-30 23:20:30 +0800

    Update Display.py

--- Display.py
+++ Display.py
@@ -1,7 +1,7 @@
 """from Book import Book"""
 
 class Display:
-
+    # this function for printing content of object
     def display_books(self,list):
         print(f"{'Title': <20}{'Author': <20}{'Genre': <20}{'Publication Year': <20}{'Pages': <20}{'Publisher': <20}{'Language': <20}{'ISBN': <20}")
 
@@ -10,6 +10,6 @@ class Display:
             print(f"{list[item].getter(0): <20}{list[item].getter(1): <20}{list[item].getter(2): <20}{list[item].getter(3): <20}{list[item].getter(4): <20}{list[item].getter(5): <20}{list[item].getter(6): <20}{list[item].getter(7): <20}")
             print('\n')
 
-
+    # i will finish it later
     def display_grid():
         pass
