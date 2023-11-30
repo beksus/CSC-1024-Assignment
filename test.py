ecbc4efe4b3d6f418e4b8312940bde8dfd81e136
beksus <85835904+beksus@users.noreply.github.com>
2023-11-30 23:13:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ file = open("book_list.txt", "r")
 
 data = file.readlines()
 data
+
+# this list is to save text content as a string format in 2d array
 new_list = []
 
 
@@ -21,10 +23,12 @@ for line in data:
     line_split = line_strip.split(',')
     new_list.append(line_split)
 
+
+#this list for carrying object 
 list = []
 
 
-
+# creating object from list taking the values and saving it to the object as Book
 for i in range(len(new_list)):
      list.append(Book(new_list[i][0] ,new_list[i][1],new_list[i][2],new_list[i][3],new_list[i][4],new_list[i][5],new_list[i][6],new_list[i][7], ))
 
@@ -32,7 +36,7 @@ for i in range(len(new_list)):
         new_list[item] = Book(new_list[item])
          """
 
-
+# this is how the new list look like as it is saved string in 2d array
 for item in new_list:
     print(item)
 
@@ -42,7 +46,7 @@ for item in new_list:
     print(list[item].getter(0) + ' | ' + list[item].getter(1) + ' | ' + list[item].getter(2) + ' | ' + list[item].getter(3) + ' | '+list[item].getter(4) + ' | '+list[item].getter(5) + ' | '+list[item].getter(6) + ' | '+list[item].getter(7) )
     print('\n')"""
 
-
+# displayer
 disp = Display()
 
 disp.display_books(list)
