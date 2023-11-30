11b91b2036411548d568e3e3f4645c50d3f7e521
beksus <85835904+beksus@users.noreply.github.com>
2023-11-30 23:20:25 +0800

    Update Book.py

--- Book.py
+++ Book.py
@@ -14,10 +14,6 @@ class Book:
     list=[]
     
     # this is constructor for initialising and saving values of Book
-
-    
-
-
     def __init__(self, isbn, author, title, publisher, genre, date_published, date_purchased, status):
         self.isbn = isbn
         self.author = author
@@ -38,8 +34,11 @@ class Book:
         self.list = [self.isbn, self.author,self.title,self.publisher,self.date_published,self.date_purchased,self.status]
 
         
-
-        
+    # This is the __str__ function that represents the object as a string
+    # in development
+    """def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Publisher: {self.publisher}, Genre: {self.genre}, Date Published: {self.date_published}, Date Purchased: {self.date_purchased}, Status: {self.status}"
+        """
 
 
     # I don't know How to use it, it was generated from BlackBox AI ,so I don't know how to utilize it
