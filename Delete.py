2f33765afd2eb17b463d66253ce1108c82e2eec9
beksus <85835904+beksus@users.noreply.github.com>
2023-12-06 16:04:27 +0800

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -5,12 +5,11 @@ class Delete:
     # this function is for deleting the books 
     def delete_book(self, book_list, book):
         #de = str(input("enter book to delete: "))
-        try:
-            for i in book_list:
-                if i.getter(7) == book or i.getter(0) == book or i.getter(1) == book:
-                    book_list.remove(i)
-        except Exception:
-            print("this book doesn't exist")
-        else:
-            print('deleted')
+        
+        for i in book_list:
+            if i.getter(7) == book or i.getter(0) == book or i.getter(1) == book:
+                book_list.remove(i)
+                return 1
+        # if there is no book exists
+        return -1
                 
